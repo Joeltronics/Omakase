f55289de94d3ed2919c0aaad8e0050727bccfbec
Joel Geddert <Joeltronics@users.noreply.github.com>
2022-12-24 21:51:00 -0800

    Change Card to IntEnum

--- cards.py
+++ cards.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python
 
 
-from enum import Enum, unique
+from enum import IntEnum, unique
 from typing import Any, Dict, Iterable, List, Union, Tuple, Optional
 
 
 @unique
-class Card(Enum):
+class Card(IntEnum):
 
-	Unknown = '?'
+	Sashimi = 1
+	Tempura = 2
+	Dumpling = 3
+	SquidNigiri = 4
+	SalmonNigiri = 5
+	EggNigiri = 6
+	Wasabi = 7
+	Maki3 = 8
+	Maki2 = 9
+	Maki1 = 10
+	Pudding = 11
+	Chopsticks = 12
 
-	Tempura = 'Tempura'
-	Sashimi = 'Sashimi'
-	Dumpling = 'Dumpling'
-	Maki1 = '1 Maki'
-	Maki2 = '2 Maki'
-	Maki3 = '3 Maki'
-	EggNigiri = 'Egg Nigiri'
-	SalmonNigiri = 'Salmon Nigiri'
-	SquidNigiri = 'Squid Nigiri'
-	Wasabi = 'Wasabi'
-	Pudding = 'Pudding'
-	Chopsticks = 'Chopsticks'
+	Unknown = 99
 
 	def __str__(self) -> str:
-		return self.value
+		return {
+			Card.Unknown: '?',
+			Card.Tempura: 'Tempura',
+			Card.Sashimi: 'Sashimi',
+			Card.Dumpling: 'Dumpling',
+			Card.Maki1: '1 Maki',
+			Card.Maki2: '2 Maki',
+			Card.Maki3: '3 Maki',
+			Card.EggNigiri: 'Egg Nigiri',
+			Card.SalmonNigiri: 'Salmon Nigiri',
+			Card.SquidNigiri: 'Squid Nigiri',
+			Card.Wasabi: 'Wasabi',
+			Card.Pudding: 'Pudding',
+			Card.Chopsticks: 'Chopsticks',
+		}[self]
 
 	def is_maki(self) -> bool:
 		return self in [Card.Maki1, Card.Maki2, Card.Maki3]
@@ -43,27 +57,8 @@ class Card(Enum):
 			return None
 
 
-def card_sort_order(card):
-	return {
-		Card.Sashimi: 1,
-		Card.Tempura: 2,
-		Card.Dumpling: 3,
-		Card.SquidNigiri: 4,
-		Card.SalmonNigiri: 5,
-		Card.EggNigiri: 6,
-		Card.Wasabi: 7,
-		Card.Maki3: 8,
-		Card.Maki2: 9,
-		Card.Maki1: 10,
-		Card.Pudding: 11,
-		Card.Chopsticks: 12,
-
-		Card.Unknown: 99,
-	}[card]
-
-
 def sort_cards(cards: Iterable[Card]) -> List[Card]:
-	return sorted(list(cards), key=card_sort_order)
+	return sorted(list(cards))
 
 
 def card_names(cards: Iterable[Union[Card, Tuple[Card, Card]]], sort=False):
@@ -73,7 +68,7 @@ def card_names(cards: Iterable[Union[Card, Tuple[Card, Card]]], sort=False):
 
 	# TODO: if sorted, count uniques - e.g. display "[2 Tempura]" instead of "[Tempura, Tempura]"
 
-	display_list = sorted(list(cards), key=card_sort_order) if sort else cards
+	display_list = sorted(list(cards)) if sort else cards
 
 	return "[" + ", ".join([str(card) for card in display_list]) + "]"
 
